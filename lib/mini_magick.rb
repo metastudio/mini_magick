cf21ca99c2a43b6ebc504d2ee63a77e9ed88ba38
Andrey Chernih <andrey.chernih@gmail.com>
2011-08-09 11:15:22 +0400

    remove pings

--- lib/mini_magick.rb
+++ lib/mini_magick.rb
@@ -175,7 +175,7 @@ module MiniMagick
     #
     # @return [Boolean]
     def valid?
-      run_command("identify", @path)
+      # run_command("identify", @path)
       true
     rescue MiniMagick::Invalid
       false
@@ -288,7 +288,7 @@ module MiniMagick
     def write(output_to)
       if output_to.kind_of?(String) || !output_to.respond_to?(:write)
         FileUtils.copy_file @path, output_to
-        run_command "identify", output_to.to_s.inspect # Verify that we have a good image
+        # run_command "identify", output_to.to_s.inspect # Verify that we have a good image
       else # stream
         File.open(@path, "rb") do |f|
           f.binmode
